09facdb1aa171b0bac82c190b6c16c157e03184f
Nick <reshyez@gmail.com>
2026-07-31 17:35:45 +0300

    add debounce helper

---
+++ app/services/helpers/debounce/debounce.js
@@ -0,0 +1,10 @@
+export function debounce(fn, delay) {
+    let timerId = null;
+
+    return function (...args) {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            fn.apply(this, args);
+        }, delay);
+    };
+}
